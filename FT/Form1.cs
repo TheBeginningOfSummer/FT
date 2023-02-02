2c13aeb07010115bbce2002ca7cf09fe6e32b2c2
Sakura <chuyindao@outlook.com>
2023-02-02 21:52:55 +0800

    改进报警检测功能

--- FT/Form1.cs
+++ FT/Form1.cs
@@ -707,25 +707,31 @@ namespace FT
                     try
                     {
                         Thread.Sleep(300);
-                        warning.Clear();//测试
                         for (int i = 0; i < communication.ReadPLCAlarm.Length; i++)
                         {
                             if (communication.ReadPLCAlarm[i])
                             {
-                                //MessageBox.Show(alarmInformation[i.ToString()], "报警信息", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                                //如果当前报警列表不包含检测到的字符串
                                 if (!warning.Contains(alarmInformation[i.ToString()]))
                                 {
                                     TB_Warning.Invoke(new Action(() => TB_Warning.Clear()));
                                     warning.Add(alarmInformation[i.ToString()]);
                                     //显示到控件上
                                     foreach (var item in warning)
-                                    {
-                                        TB_Warning.Invoke(new Action(() =>
-                                        {
-                                            TB_Warning.AppendText(item + Environment.NewLine);
-                                        }));
-                                    }
+                                        TB_Warning.Invoke(new Action(() => TB_Warning.AppendText(item + Environment.NewLine)));
                                     logfile.WriteLog(alarmInformation[i.ToString()], "报警记录");
+                                }
+                            }
+                            else
+                            {
+                                //如果当前报警列表包含已经清除的报警
+                                if (warning.Contains(alarmInformation[i.ToString()]))
+                                {
+                                    TB_Warning.Invoke(new Action(() => TB_Warning.Clear()));
+                                    warning.Remove(alarmInformation[i.ToString()]);
+                                    //更新显示内容
+                                    foreach (var item in warning)
+                                        TB_Warning.Invoke(new Action(() => TB_Warning.AppendText(item + Environment.NewLine)));
                                 }
                             }
                         }
